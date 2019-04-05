2fbc236a52a1272bd1cd84299fccde99e99dce5c
Sergio Rodriguez <48034403+SergioR22@users.noreply.github.com>
2019-04-04 19:57:08 -0600

    Clase02042019

---
+++ Clase02042019/Clase02042019/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clase02042019
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            progressBar1.Maximum = 100;
+            progressBar1.Minimum = 0;
+            progressBar1.Step = 4;
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i <= 24; i++)
+            {
+
+                progressBar1.PerformStep();
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           
+                progressBar1.PerformStep();
+                Form2 frm = new Form2();
+                frm.Show();
+                this.Hide();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i <= 24; i++)
+            {
+
+                progressBar1.PerformStep();
+            }
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            button2.BackColor = Color.Green;
+            timer1.Interval = 3000;
+            timer1.Enabled = true;
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Form2 frm = new Form2();
+            frm.Show();
+        }
+    }
+}
